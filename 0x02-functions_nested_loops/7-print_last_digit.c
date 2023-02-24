7611a275cdc6b841b3311099629d1944b067dbd2
yeabsira11 <yabubetty22@gmail.com>
2023-02-24 04:34:17 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - function that prints the last digit of a number
+ * print_last_digit - function that prints the last digit
  * Return: value of the last digit
  */
 int print_last_digit(int n)
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-* jack_bauer -  I'm federal agent Jack Bauer, and today is the longest day of my
-*  life, a function that prints every minute of the day of Jack Bauer,
-*   starting from 00:00 to 23:59
+ * jack_bauer -  I'm federal agent Jack Bauer,
+ * and today is the longest day of my
+ * life, a function that prints every minute of the day
+ * of Jack Bauer,
+ * starting from 00:00 to 23:59
 */
 void jack_bauer(void)
 {
