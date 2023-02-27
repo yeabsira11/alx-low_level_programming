4bf0d12ccdea267aaaa37e2342e8e20011b5bcca
yeabsira11 <yabubetty22@gmail.com>
2023-02-27 05:48:29 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,14 @@
  * main - testing a function that prints if integer is positive
  * Return: 0
  */
-int main(void)
 
+int main(void)
 {
 	int i;
 
-	i=0;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
 }
+
