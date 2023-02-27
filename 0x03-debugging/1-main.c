69478235deb9cb9d212ded8f5df3840f6f076d3e
yeabsira11 <yabubetty22@gmail.com>
2023-02-27 06:04:57 -0800

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop :(\n");
+
+	i =0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*     putchar(i);*/
+	/*}*/
+
+	printf("Avoide infinite loop! \\o/\n");
+
+	return (0);
+}
